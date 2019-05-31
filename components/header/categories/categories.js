425829713e93bdb45380ba8b043d9df96ba0c8cc
jordans <jordan.schuringa@mnfgroup.limited>
2019-05-31 13:02:25 +1000

    Added overlay transition

--- components/header/categories/categories.js
+++ components/header/categories/categories.js
@@ -26,61 +26,69 @@ export default class Categories extends Component {
   render () {
     const { displayCategories } = this.state
     return (
-      <div className="column is-paddingless">
+      <React.Fragment>
         <div
-          onMouseLeave={this.hideCategories}
-          className="navbar-menu category-tab"
-        >
-          <div className="tabs is-marginless">
-            <ul>
-              <li
-                className={`is-size-7 ${displayCategories ? 'is-active' : ''}`}
-              >
-                <a
-                  href="/"
-                  onMouseEnter={this.showCategories}
-                  className="is-size-7"
-                >
-                  Categories
-                </a>
-              </li>
-            </ul>
-          </div>
+          style={{ opacity: `${displayCategories ? '1' : '0'}` }}
+          className="overlay-desktop"
+        />
+        <div className="column is-paddingless">
           <div
-            style={{ height: `${displayCategories ? '250px' : '0'}` }}
-            onMouseEnter={this.showCategories}
-            className="categories"
+            onMouseLeave={this.hideCategories}
+            className="navbar-menu category-tab"
           >
-            <div className="columns is-multiline">
-              {data.topnavi.navigation_node.children[2].children.map(
-                (value, index) => {
-                  return (
-                    <div key={index} className="column category-list">
-                      <p>
-                        <a
-                          href={value.url_key}
-                          className="has-text-weight-bold is-size-7"
-                        >
-                          {value.name}
-                        </a>
-                      </p>
-                      {value.children.map((value, index) => {
-                        return (
-                          <p key={index}>
-                            <a href={value.url_key} className="is-size-7">
-                              {value.name}
-                            </a>
-                          </p>
-                        )
-                      })}
-                    </div>
-                  )
-                }
-              )}
+            <div className="tabs is-marginless">
+              <ul>
+                <li
+                  className={`is-size-7 ${
+                    displayCategories ? 'is-active' : ''
+                  }`}
+                >
+                  <a
+                    href="/"
+                    onMouseEnter={this.showCategories}
+                    className="is-size-7"
+                  >
+                    Categories
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div
+              style={{ height: `${displayCategories ? '250px' : '0'}` }}
+              onMouseEnter={this.showCategories}
+              className="categories"
+            >
+              <div className="columns is-multiline">
+                {data.topnavi.navigation_node.children[2].children.map(
+                  (value, index) => {
+                    return (
+                      <div key={index} className="column category-list">
+                        <p>
+                          <a
+                            href={value.url_key}
+                            className="has-text-weight-bold is-size-7"
+                          >
+                            {value.name}
+                          </a>
+                        </p>
+                        {value.children.map((value, index) => {
+                          return (
+                            <p key={index}>
+                              <a href={value.url_key} className="is-size-7">
+                                {value.name}
+                              </a>
+                            </p>
+                          )
+                        })}
+                      </div>
+                    )
+                  }
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 }
