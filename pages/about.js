ec193b2ebc29dcb946e252b50f8d6e7a8d341f5d
jordans <jordan.schuringa@mnfgroup.limited>
2019-06-04 13:41:30 +1000

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -8,21 +8,12 @@ export default () => (
       <div className="column has-text-centered">
         <h3 className="title is-size-6">About</h3>
         <p className="is-size-7">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          tincidunt, purus sed semper luctus, justo tortor dictum quam, cursus
-          pharetra urna urna nec odio. Nullam ac sagittis nibh. Aenean nisi
-          purus, accumsan mattis placerat eget, ultrices eget orci. Sed
-          tincidunt, diam id volutpat efficitur, elit erat mattis elit,
-          convallis suscipit lectus lectus iaculis justo. In ac dictum erat.
-          Praesent non diam vel felis tristique tempor vel non massa. Morbi
-          lobortis nulla vel tortor fermentum placerat. Mauris volutpat ut velit
-          a tincidunt. Etiam in odio tempor, faucibus tortor vel, ornare est.
-          Quisque pulvinar leo quis hendrerit auctor. Donec nisl sem,
-          sollicitudin at porttitor eu, malesuada ac ex. Pellentesque habitant
-          morbi tristique senectus et netus et malesuada fames ac turpis
-          egestas. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Nulla accumsan nisl placerat erat
-          auctor aliquam.
+          You are one of the first users of the newly opened PriceHelp. Our
+          service allow you to choose from a majority of all United Kingdom’s
+          retailers and brands, helping you find the best product for your need
+          with ease. We have many new features to be released to stay tuned.
+          Your feedback is always appreciated, so please feel free to contact
+          us.
         </p>
       </div>
     </div>
