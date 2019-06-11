e8ae3750884e753991581434a2f709d632613a4f
jordans <jordan.schuringa@mnfgroup.limited>
2019-06-11 14:31:54 +1000

    Added image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withSass = require('@zeit/next-sass')
+const optimizedImages = require('next-optimized-images')
 const config = {
   target: 'serverless'
 }
-module.exports = withSass(config)
+module.exports = withSass(config, optimizedImages({ imagesFolder: 'static' }))
