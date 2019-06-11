96cc7b30ccfddd1b345f377cd2241ecacbb5a812
jordans <jordan.schuringa@mnfgroup.limited>
2019-06-11 12:59:43 +1000

    Added fontpage

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,70 @@ class IndexPage extends React.Component {
   render () {
     return (
       <Default meta={meta}>
-        <div>MAIN PAGE!</div>
+        <section className="hero has-text-centered">
+          <div className="hero-body banner">
+            <div className="container">
+              <h1 className="title has-text-weight-normal">
+                Sustainable shopping starts at Price
+                <span className="has-text-success">Help</span>
+              </h1>
+              <h2 className="subtitle">
+                Search and discover millions of products, used and new, local
+                and international from the whole internet
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section className="hero card">
+          <img
+            className="banner-image"
+            src="/static/forest_banner.jpg"
+            alt="Forest banner"
+          />
+          <div className="columns banner">
+            <div className="column has-text-centered">
+              <div>
+                <p className="heading">You can buy everything</p>
+                <p className="is-size-7">12 million goods from UK merchants</p>
+              </div>
+            </div>
+            <div className="column has-text-centered">
+              <div>
+                <p className="heading">Best prices</p>
+                <p className="is-size-7">Find the cheapest</p>
+              </div>
+            </div>
+            <div className="column has-text-centered">
+              <div>
+                <p className="heading">Protect the environment</p>
+                <p className="is-size-7">
+                  Product that last long & have sustainable production
+                </p>
+              </div>
+            </div>
+            <div className="column has-text-centered">
+              <div>
+                <p className="heading">Reliable merchants</p>
+                <p className="is-size-7">
+                  Passed the PriceHelp sustainability test
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <style jsx>
+          {`
+            .banner {
+              padding: 1em;
+            }
+
+            .banner-image {
+              object-fit: cover;
+              height: 35vh;
+              width: 100%;
+            }
+          `}
+        </style>
       </Default>
     )
   }
