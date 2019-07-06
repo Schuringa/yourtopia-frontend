ce7b5855c075b3068ac09b483ebc66be7bf18880
schuringa <schuringa.jordan@gmail.com>
2019-07-06 21:45:40 +1000

    update inital product count

--- pages/products.js
+++ pages/products.js
@@ -23,7 +23,7 @@ class ProductsPage extends React.Component {
               category: { $regex: `${query.category}` }
             },
             skip: 0,
-            limit: 20,
+            limit: 21,
             token:
               'eyJhbGciOiJSU0EtT0FFUCIsImN0eSI6IkpXVCIsImVuYyI6IkExMjhHQ00iLCJ0eXAiOiJKV1QifQ.xjSimwpZHyHjEwhPrj4rYxkyHvplK1KWMyWuJOeEHz3C6p8Ee3b46CwFgPoy46T7LRXOf4PsKCYNBK_YzIiTic4o6YT3ykpupi_Auz6TAPIA70Yt0XTEkcav0313cHGDPnWcj3GOIzLptjAN70NskhjYoHZhP-3hBg017a24VlFTehaXuQPhB5xIqTU3UiX-fyhQjcDnI4C8R5NO_T7MX0dgIaSXgqU-wpQWpdwKnDkKRolZ3-3YMYb6BL-4DFxinbeMmd2U12rS7s8H0FXLHQwwq1muz58vSlxTCedVUhOfBr1sMQ4EgS3ipdkXB2cw5kle48PeQbQHJGM_n-QkWQ.Vs54Iok1evhDwc6_.PfxX_pCXznZ9T3HTTq6FwP8sliop36W-zXDFMG6Xo96dznOSSwaTHCJQz5ZlClvtrHpmGezi8MOOJmMJDClC-P35LYdwxsYHszXY0GbTZW505aKSLCGzqKBhLEM34I9HwXdQGSrifse8WFKdh7rP_9NUGzCCu-RyA6c8DEtNUXXnLcgibkRJrVqMpNju5ACElc6I-P5oPWeG7G7fGwNfwOdG1cVVrMWaQrELAdP9pAnIxK4Rd1N4Eb-tbPaGvr_zCoerGS-wsFrCWkpFgLAZ3OWv73RiQaZQjeviw15xGVfXXXFZGEuThP4aaMHYKKHnxjmM-0a-1RGfXCgcPskOZByKZGn8fom0qygqNHiRs3gFz45cU3eprB9k__f_fpF7__7-aCd3cGv_6tlfV5PwYaJ-66X3r8OTSdn0jdj1zHJf8w-0AOm9_EHQWkT1pOaqC37XaStDC4b7DZK11QiEIANIbVwZAI4_2p77tlcGMfdLikmLMD76S2N4BBPAK4vHSf_Ev6-Bf2YBDigBBBJf9x2yp1G22o2qM3H16rswTvcav-Ar4ZcblbD4mJGBM8Hb14qRjFxywS_hiRauxcxOtd1IHIxG7xwJdhwL98sBbiomNOzfwaHJl1CnZ-zo0c3_r2a8tzZEWwBoHT9dmSrvIJWaXnlupYS6XiRVMyIQPKueVctSKs_5yLiowniOPBKhimLrKezK4fcJGPGdjYSfUQKCHfNU7uFbaZ3cjlqNWWfjWRnKJDEcQvTKnHDUqCjXkPuYYebDD0mVMyUDbCe6COCUmYSGdEGXG9SoDkP3CuN15tmt7Do--lQTkOmAUIaE54IPhn7nteOG9LNNQAaeDozJI3gMO6UcjYxTpqkxbn6fd3taLrkrZfl-apjWI6QS.fq-EwgZntDizV1i8-x6L7w'
           }
@@ -45,7 +45,7 @@ class ProductsPage extends React.Component {
   state = {
     products: this.props.products,
     currentPage: 1,
-    limit: 20
+    limit: 21
   }
 
   async loadMoreProducts () {
